aede2d32b21837767f437b8a49e405cbb95daa20
lyubomyr83 <lyubomyr83@mail.ru>
2019-10-22 16:59:28 +0300

    game guess number

--- rodjer.py
+++ rodjer.py
@@ -11,6 +11,23 @@ while answer not in {'да', 'нет'}:
     answer = input()
 if answer == 'да':
     print('Хорошо давай начнем')
+    question_quantity = ''
+
+    while not question_quantity.isdigit():
+        print(name + ', сколько примеров ты готов решить?')
+        question_quantity = input()
+
+        if question_quantity.isdigit():
+            while int(question_quantity) < 1:
+                print('Должно быть больше "0" ')
+                question_quantity = input()
+                while not question_quantity.isdigit():
+                    print('Должна быть цифра!!!')
+                    question_quantity = input()
+        else:
+            print('Должна быть цифра')
+    print('До скольки будем считать? Например до 100')
+
 else:
     print('''Передумал, зря!
 Пока!''')
